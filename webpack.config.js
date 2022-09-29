946aa45120f6fd149985191e4f527756ecf1e19f
Alexander Vasiliev <asvasi25@gmail.com>
2022-09-29 08:57:24 +0300

    refactor: move html template from src

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ export default {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './index.html',
     }),
     new MiniCssExtractPlugin(),
   ],
